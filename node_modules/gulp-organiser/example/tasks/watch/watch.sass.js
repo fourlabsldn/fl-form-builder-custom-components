a0a09fde94eeadf1200a7d5a06d7be6df9dee9fc
lazamar <lazaronijunior@gmail.com>
2016-10-13 17:11:04 +0100

    Remove node_modules from tracking

--- node_modules/gulp-organiser/example/tasks/watch/watch.sass.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-const sass = require('../sass');
-const organiser = require('gulp-organiser');
-
-module.exports = organiser.register((task) => {
-  gulp.task(task.name, () => gulp.watch(sass.src, [sass.name]));
-});
